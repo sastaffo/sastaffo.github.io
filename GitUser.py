2e77485576ed9fe1fe2eb43a9128d38bcf950a64
sastaffo <sastaffo@tcd.ie>
2018-12-01 14:18:31 +0000

    adding SuperUser class

--- GitUser.py
+++ GitUser.py
@@ -3,12 +3,11 @@ import json
 
 from helper import Geocoder
 from helper import JSONBuilder
+from helper import Decode
 
-lines = [line.rstrip('\n') for line in open("token.txt")]
-auth = lines[0]
-
+googleCipher = "cn_CrcKSwonCrHdrfWzCrWnCpGDCjsKzwohtZ3XCncKMwoXCiWnCn2jCm8KuworChH7Cm8KHwoLCpcKOwo3Cpw=="
+githubCipher = "YsKYwpRhwpjClGvCm2dpwpVhwpvClmTCnsKSbmVib8KYbMKdaGdpYcKcaGVswpXCm2dmwpvCl2rCnQ=="
 geocoder = Geocoder()
-jsonb = JSONBuilder()
 
 
 class User:
@@ -21,19 +20,21 @@ class User:
 		self.tuples_list = [] # array of tuples: (loc1,lng1,lat1,loc2,lng2,lat2)
 	# END init
 
-	def get_location(self):
-		url = ("https://api.github.com/users/"+self.username+auth)
+	def get_location(self, githubKey, googleKey):
+		url = ("https://api.github.com/users/"+self.username+githubKey)
 		req = urllib.request.Request(url)
 		with urllib.request.urlopen(req) as resp:
 			s = resp.read()
 		user_json = json.loads(s)
 		self.location = user_json["location"]
 		if self.location is not None:
-			self.location_tuple = geocoder.get_lnglat(self.location)
+			self.location_tuple = geocoder.get_lnglat(self.location, googleKey)
+		if self.location_tuple == (0,0):
+			self.location = None
 	# END get_location
 
-	def get_followers(self):
-		url = ("https://api.github.com/users/"+self.username+"/followers"+auth)
+	def get_followers(self, githubKey, googleKey):
+		url = ("https://api.github.com/users/"+self.username+"/followers"+githubKey)
 		req = urllib.request.Request(url)
 		with urllib.request.urlopen(req) as resp:
 			s = resp.read()
@@ -42,7 +43,7 @@ class User:
 		for fol_json in fol_list:
 			name = fol_json["login"]
 			u = User(name)
-			u.get_location()
+			u.get_location(githubKey, googleKey)
 			if u.location_tuple != (0,0):
 				self.followers.append(u)
 				print('.', end='') # prints dots to show progress
@@ -50,9 +51,32 @@ class User:
 		self.makelist()
 	# END get_followers
 
+	def makelist(self):
+		self_tuple = (self.location, self.location_tuple[0], self.location_tuple[1])
+		for u in self.followers:
+			u_tuple = (u.location, u.location_tuple[0], u.location_tuple[1])
+			self.tuples_list.append(self_tuple+u_tuple)
+		# END for
+	# END makelist
+# END User
+
+class SuperUser (User):
+	def __init__(self, username, key):
+		super(SuperUser, self).__init__(username)
+		self.key = key
+		self.dec = Decode(key)
+		self.googleClear = self.dec.decode(googleCipher)
+		githubClear = self.dec.decode(githubCipher)
+		self.githubClear = ("?access_token="+githubClear)
+		self.jsonb = JSONBuilder()
+	# END init
+	def get_followers(self):
+		super(SuperUser, self).get_followers(self.githubClear, self.googleClear)
+	# END get_followers
+
 	def get_2nd_deg_fols(self):
 		for f in self.followers:
-			f.get_followers()
+			f.get_followers(self.githubClear, self.googleClear)
 			for f2 in f.followers:
 				self.fols_2nd_degree.append((f,f2))
 				print('.', end='')
@@ -64,27 +88,23 @@ class User:
 	# END get_2nd_deg_fols
 
 	def get_all(self):
-		self.get_location()
+		self.get_location(self.githubClear, self.googleClear)
 		if self.location is None:
 			return
 		self.get_followers()
-		#self.get_2nd_deg_fols()
-		jsonb.build(self.tuples_list)
+		self.get_2nd_deg_fols()
+		self.jsonb.build(self.tuples_list)
 	# END get_all
-
-	def makelist(self):
-		self_tuple = (self.location, self.location_tuple[0], self.location_tuple[1])
-		for u in self.followers:
-			u_tuple = (u.location, u.location_tuple[0], u.location_tuple[1])
-			self.tuples_list.append(self_tuple+u_tuple)
-		# END for
-	# END makelist
-# END User
+# END SuperUser
 
 def main():
-	username = "aforemny"
-	# lydell is a Swedish software desginer with approx 100 followers
-	start = User(username)
+	key = input("Enter password: ")
+	username = input("Enter username (leave blank for default): ")
+	if username == "":
+		username = "lydell"
+		# lydell is a Swedish software desginer with approx 100 followers
+	# END if
+	start = SuperUser(username, key)
 	start.get_all()
 	# END for
 # END main
